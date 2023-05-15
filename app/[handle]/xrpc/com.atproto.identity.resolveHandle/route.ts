e4e8f26938872c1f5dfb20056b8000a2640780c8
Samuel Newman <mozzius@protonmail.com>
2023-05-15 16:00:19 +0100

    forgot to make the most important part work!!!

--- app/[handle]/xrpc/com.atproto.identity.resolveHandle/route.ts
+++ app/[handle]/xrpc/com.atproto.identity.resolveHandle/route.ts
@@ -1,12 +1,14 @@
 import { NextResponse, type NextRequest } from "next/server"
+import { kv } from "@vercel/kv"
 
 export const GET = async (
   request: NextRequest,
   { params }: { params: { handle: string } }
 ) => {
-  const { handle } = params
+  const value = await kv.get(params.handle + "." + process.env.DOMAIN)
+  if (!value || typeof value !== "string") throw new Error("not in kv")
 
   return NextResponse.json({
-    did: "did:plc:p2cp5gopk7mgjegy6wadk3ep",
+    did: value,
   })
 }
