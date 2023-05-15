daf4bc451a3777820b3bf2754e5adcd934ce2103
Samuel Newman <mozzius@protonmail.com>
2023-05-15 12:00:17 +0100

    add config matcher

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,23 @@ import { NextResponse, type NextRequest } from "next/server"
 
 const domain = process.env.DOMAIN as string
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const url = new URL(request.url)
-  
+
   if (url.hostname !== domain) {
     const subdomain = url.hostname.slice(0, -domain.length - 1)
     return NextResponse.rewrite(new URL(`/${subdomain}${url.pathname}`, url))
   }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api/|_next/|_static/|[\\w-]+\\.\\w+).*)",
+  ],
 }
