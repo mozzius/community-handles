4e6988c0a095f8221c4d42e192f9de393f2ee6d2
Samuel Newman <mozzius@protonmail.com>
2023-05-16 17:12:47 +0100

    Update opengraph-image.tsx

--- app/[handle]/opengraph-image.tsx
+++ app/[handle]/opengraph-image.tsx
@@ -5,8 +5,8 @@ import { kv } from "@vercel/kv"
 import { getAgent } from "@/lib/atproto"
 
 export const size = {
-  width: 1200,
-  height: 630,
+  width: 800,
+  height: 400,
 }
 export const contentType = "image/png"
 export const runtime = "edge"
@@ -23,11 +23,50 @@ export default async function og({ params }: { params: { handle: string } }) {
   const profile = await agent.getProfile({
     actor: value,
   })
+
+  const fetchAvatar = profile.data.avatar
+    ? fetch(profile.data.avatar).then((res) => res.arrayBuffer())
+    : Promise.resolve(null)
+  const fetchBanner = profile.data.banner
+    ? fetch(profile.data.banner).then((res) => res.arrayBuffer())
+    : Promise.resolve(null)
+
+  const [avatar, banner] = await Promise.all([fetchAvatar, fetchBanner])
+
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full">
-        <img src={profile.data.banner} tw="w-full h-1/3 object-cover" alt="" />
-        
+      <div tw="flex flex-col w-full h-full bg-white">
+        {banner && (
+          <img // @ts-expect-error img behaves differently here
+            src={banner}
+            height="200"
+            width="800"
+            alt=""
+          />
+        )}
+        <div tw="flex flex-row items-center flex-1 px-12">
+          {avatar ? (
+            <img // @ts-expect-error img behaves differently here
+              src={avatar}
+              height="150"
+              width="150"
+              tw="rounded-full"
+              alt=""
+            />
+          ) : (
+            <div tw="rounded-full bg-neutral-200 h-36 w-36" />
+          )}
+          <div tw="flex flex-col ml-6">
+            {profile.data.displayName && (
+              <p tw="m-0 text-4xl font-bold w-[500px]">
+                {profile.data.displayName}
+              </p>
+            )}
+            <p tw="mt-2 mb-0 text-neutral-500 text-2xl w-[500px]">
+              @{profile.data.handle}
+            </p>
+          </div>
+        </div>
       </div>
     ),
     { ...size }
