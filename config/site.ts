70b6cba530c4cb3556a1b5cf014c2850f90c35a8
Samuel Newman <mozzius@protonmail.com>
2023-05-17 13:34:39 +0100

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -13,7 +13,7 @@ export const siteConfig = {
       href: "/community",
     },
     {
-      title: "Create your own community handle tool",
+      title: "Create your own community handle",
       href: "/create-your-own",
     },
   ],
