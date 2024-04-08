ca2af0ad573cc60e3e249c25461d9d7153f6114b
Samuel Newman <mozzius@protonmail.com>
2024-04-08 11:50:02 +0100

    try and center spinner

--- app/[domain]/community/loading.tsx
+++ app/[domain]/community/loading.tsx
@@ -2,8 +2,8 @@ import { LoaderIcon } from "lucide-react"
 
 export default function Loading() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <LoaderIcon className="h-12 w-12 animate-spin text-primary" />
+    <div className="flex-1 flex items-center justify-center">
+      <LoaderIcon className="h-8 w-8 animate-spin text-primary" />
     </div>
   )
 }
