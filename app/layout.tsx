99d47c85899e184ef3004246b727b700fe4323eb
Samuel Newman <mozzius@protonmail.com>
2024-04-08 10:07:19 +0100

    move themecolor to viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
-import "@/styles/globals.css"
 import { Analytics } from "@vercel/analytics/react"
+import { type Metadata,type  Viewport } from "next"
 
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
-export const metadata = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
+import "@/styles/globals.css"
+
+export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
+}
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 }
 
 interface RootLayoutProps {
