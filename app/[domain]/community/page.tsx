3f7152156f481e857b829d5df5ede448cd29aa08
Tales Oliveira <tales.oliveira@alunos.fho.edu.br>
2024-09-03 11:17:03 -0300

    testing

--- app/[domain]/community/page.tsx
+++ app/[domain]/community/page.tsx
@@ -27,7 +27,7 @@ export default async function CommunityPage({ params }: Props) {
 
   const [count, { profiles: initialProfiles, nextOffset }] = await Promise.all([
     prisma.user.groupBy({
-      by: ['user.did'],
+      by: ['did'],
       where: { domain: { name: domain } },
       _count: {
         _all: true,
