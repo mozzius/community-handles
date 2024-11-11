dc107df0bac7a44d65dad686da83fbcf21a56dd6
denys <orion763357@gmail.com>
2024-11-11 17:32:52 -0400

    add Ukrain flag

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,12 +11,15 @@ const subdomain = "testing"
 export function getDomain(host: string) {
   const domain = psl.parse(host)
   if (domain.error) throw new Error(domain.error.message)
-  if (domain.subdomain?.endsWith(subdomain)) {
+  if (
+    domain.subdomain?.endsWith(subdomain) ||
+    !domain.domain?.includes("fellas.social")
+  ) {
     domain.domain = subdomain + "." + domain.domain
     domain.subdomain =
       domain.subdomain === subdomain
         ? null
-        : domain.subdomain.replace(`.${subdomain}`, "")
+        : domain.subdomain?.replace(`.${subdomain}`, "") || null
   }
 
   return domain
