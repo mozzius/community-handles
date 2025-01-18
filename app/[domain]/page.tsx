9b28688c1576848b56d72dc6837e113acb93cc49
ChrisRid <christian@ridout.online>
2025-01-18 10:56:42 +0000

    Removed the Buy Me a Coffee link

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -66,53 +66,6 @@ export default async function IndexPage({
               />
             </a>
           </div>
-          <div className="max-w-lg text-center">
-            <p className="text-sm">
-              {t("To help support the hosting costs for this service")}:
-            </p>
-            <p className="py-2 text-center">
-              <a
-                href="https://buymeacoffee.com/chrisrid"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                buymeacoffee.com/chrisrid
-              </a>
-            </p>
-            <p>({t("Please prioritize")})</p>
-            <div className="py-4">
-              <a
-                target="_blank"
-                href="https://buymeacoffee.com/chrisrid"
-                className="inline-block bg-white p-2"
-              >
-                <Image
-                  src={ImageCoffeQR}
-                  width={300}
-                  height={300}
-                  alt="CoffeQR"
-                />
-              </a>
-            </div>
-            <p>
-              {t("Want a different NAFO related handle")}&nbsp;
-              <a
-                href="https://mainbastards.online/"
-                target="_blank"
-                className="hover:underline"
-              >
-                MainBastards.online
-              </a>
-            </p>
-            <p>{t("The community-handles project")}</p>
-            <p>{t("Ongoing maintenance")}</p>
-            <p>{t("To support")}</p>
-            <p>
-              <a href="https://github.com/sponsors/mozzius" target="_blank">
-                https://github.com/sponsors/mozzius
-              </a>
-            </p>
-          </div>
         </div>
       </div>
     </main>
